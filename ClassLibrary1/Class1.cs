42b8b3705ddceca53662a47de760e2d72520fea6
Cory Nelson <cnelson@ctlglobalsolutions.com>
2017-06-22 11:11:20 -0500

    Add test case.

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -4,5 +4,14 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public void Foo()
+        {
+            int x = 0;
+            float y = 1.0f;
+
+            var t = (x, y);
+
+            (int foo, float bar) = t;
+        }
     }
 }
